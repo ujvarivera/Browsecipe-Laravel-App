2f684b7976aa77eb7cc2dbbcbe27dfe821257c52
ujvarivera <ujvariveronika0320@gmail.com>
2023-07-13 20:40:17 +0200

    some relationship defined between recipes and categories

---
+++ app/Models/Recipe.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'description',
+        'active',
+        'user_id'
+    ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function ingredients(): HasMany
+    {
+        return $this->hasMany(RecipeIngredient::class);
+    }
+
+    public function shortDescription($words = 50): string
+    {
+        return Str::words(strip_tags($this->description), $words);
+    }
+
+    public function getImage()
+    {
+        if (str_starts_with($this->image, 'http')) {
+            return $this->image;
+        }
+
+        return '/storage/' . $this->image;
+    }
+}
